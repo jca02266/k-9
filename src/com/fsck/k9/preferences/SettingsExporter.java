6ae8434e3c3ad255f3c81746619e5aa8083fcff3
Koji Arai <jca02266@gmail.com>
2011-11-15 21:21:45 +0900

    avoid exception for decoding URI of EAS account.

--- src/com/fsck/k9/preferences/SettingsExporter.java
+++ src/com/fsck/k9/preferences/SettingsExporter.java
@@ -151,6 +151,10 @@ public class SettingsExporter {
             serializer.startTag(null, ACCOUNTS_ELEMENT);
             for (String accountUuid : exportAccounts) {
                 Account account = preferences.getAccount(accountUuid);
+                if (account.getStoreUri().startsWith("eas")) {
+                    // Not Implemented.
+                    continue;
+                }
                 writeAccount(serializer, account, prefs);
             }
             serializer.endTag(null, ACCOUNTS_ELEMENT);
