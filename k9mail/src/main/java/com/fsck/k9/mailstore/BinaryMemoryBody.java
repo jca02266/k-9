28dffd79f968e3c349a6aa69651465d044f8b488
Koji Arai <jca02266@gmail.com>
2015-11-24 23:22:33 +0900

    Workaround: Possible to convert the BinaryMemoryBody to the quoted-printable for sending.

--- k9mail/src/main/java/com/fsck/k9/mailstore/BinaryMemoryBody.java
+++ k9mail/src/main/java/com/fsck/k9/mailstore/BinaryMemoryBody.java
@@ -8,13 +8,17 @@ import java.io.OutputStream;
 
 import com.fsck.k9.mail.Body;
 import com.fsck.k9.mail.MessagingException;
+import com.fsck.k9.mail.filter.Base64OutputStream;
 import com.fsck.k9.mail.internet.RawDataBody;
 import com.fsck.k9.mail.internet.SizeAware;
+import org.apache.commons.io.IOUtils;
+import org.apache.james.mime4j.codec.QuotedPrintableOutputStream;
+import org.apache.james.mime4j.util.MimeUtil;
 
 
 public class BinaryMemoryBody implements Body, RawDataBody, SizeAware {
     private final byte[] data;
-    private final String encoding;
+    private String encoding;
 
     public BinaryMemoryBody(byte[] data, String encoding) {
         this.data = data;
@@ -32,13 +36,40 @@ public class BinaryMemoryBody implements Body, RawDataBody, SizeAware {
     }
 
     @Override
-    public void setEncoding(String encoding) throws UnavailableStorageException, MessagingException {
-        throw new RuntimeException("nope"); //FIXME
+    public void setEncoding(String encoding) throws MessagingException {
+        if (!MimeUtil.ENC_7BIT.equalsIgnoreCase(encoding)
+                && !MimeUtil.ENC_8BIT.equalsIgnoreCase(encoding)
+                && !MimeUtil.isBase64Encoding(encoding)
+                && !MimeUtil.isQuotedPrintableEncoded(encoding)) {
+            throw new MessagingException(
+                    "Incompatible content-transfer-encoding applied to a CompositeBody");
+        }
+        this.encoding = encoding;
     }
 
     @Override
     public void writeTo(OutputStream out) throws IOException, MessagingException {
-        out.write(data);
+        InputStream in = getInputStream();
+        try {
+            boolean closeStream = false;
+            if (MimeUtil.isBase64Encoding(encoding)) {
+                out = new Base64OutputStream(out);
+                closeStream = true;
+            } else if (MimeUtil.isQuotedPrintableEncoded(encoding)){
+                out = new QuotedPrintableOutputStream(out, false);
+                closeStream = true;
+            }
+
+            try {
+                IOUtils.copy(in, out);
+            } finally {
+                if (closeStream) {
+                    out.close();
+                }
+            }
+        } finally {
+            in.close();
+        }
     }
 
     @Override
